3243c8c4b155e073ae2047834ba83d5ceab6ba81
Clement <clement.beal@outlook.com>
2024-07-25 15:47:49 -0400

    test: add fixtures to help with testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,48 @@
 import pytest
 
-from talaka_kombat_jrpg.player import Player
+from talaka_kombat_jrpg.player import ButtonCombination, Player, Skill
 
 
 @pytest.fixture
-def player_1_name():
+def player_1_name() -> str:
     return "Tony"
 
 
 @pytest.fixture
-def player_1(player_1_name):
+def player_1(player_1_name) -> Player:
     return Player(player_name=player_1_name)
 
 
 @pytest.fixture
-def player_2_name():
+def player_2_name() -> str:
     return "Arnold"
 
 
 @pytest.fixture
 def player_2(player_2_name):
     return Player(player_name=player_2_name)
+
+
+class MockSkill(Skill):
+    def __init__(self) -> None:
+        super().__init__(
+            2,
+            ButtonCombination(["K"], ["A", "D"]),
+        )
+
+    def change_combination(self, new_combination: ButtonCombination):
+        self.button_combination = new_combination
+
+    def get_dammage_message(self, origin_player: Player, target_player: Player) -> str:
+        return ""
+
+
+@pytest.fixture
+def skill():
+    return MockSkill()
+
+
+@pytest.fixture
+def player_1_with_skill(player_1: Player, skill: Skill):
+    player_1.add_skill(skill)
+    return player_1
