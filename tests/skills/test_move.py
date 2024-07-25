f39e05012aacb656c4e7e77817ac046fa244bf59
Clement <clement.beal@outlook.com>
2024-07-25 15:48:04 -0400

    test: fix the move test

--- tests/skills/test_move.py
+++ tests/skills/test_move.py
@@ -1,7 +1,6 @@
-from talaka_kombat_jrpg.player import ButtonCombination
 from talaka_kombat_jrpg.skills.move import Move
 
 
 def test_move__get_dammage_message(player_1, player_2, player_1_name):
-    skill = Move(ButtonCombination([], []))
+    skill = Move()
     assert skill.get_dammage_message(player_1, player_2) == f"{player_1_name} se mueve"
