07ea424cb04d2765a2afdfb5b01b726fd404d29b
Clement <clement.beal@outlook.com>
2024-07-26 09:29:17 -0400

    test: test more the simulation

--- tests/test_fight_simulation.py
+++ tests/test_fight_simulation.py
@@ -18,6 +18,7 @@ def test_fight_simulator():
 
     assert winner is simulator.player_2
     assert winner.energy == 2
+    assert simulator.player_1.energy == 0
 
 
 def test_fight_simulator__two():
@@ -35,3 +36,23 @@ def test_fight_simulator__two():
     assert winner is simulator.player_2
     assert winner.energy == 3
     assert simulator.player_1.energy == -1
+
+
+def test_fight_simulator__three():
+    fight_data = {
+        "player1": {
+            "movimientos": ["SDD", "DSD", "SA", "DSD"],
+            "golpes": ["K", "P", "K", "P"],
+        },
+        "player2": {
+            "movimientos": ["DSD", "WSAW", "ASA", "", "ASA", "SA"],
+            "golpes": ["P", "K", "K", "K", "P", "k"],
+        },
+    }
+
+    simulator = FightSimulator()
+    winner = simulator.start_fight(fight_data)
+
+    assert winner is simulator.player_1
+    assert winner.energy == 1
+    assert simulator.player_2.energy == -2
